53678552bf4d8c6a0ea2e7cbf65701514d0d311c
IsaacDSC <56350331+IsaacDSC@users.noreply.github.com>
2019-11-01 15:06:16 -0300

    add Exercicios Total JS

---
+++ Aula de JavaScript/EXERCICIO/calculando.js
@@ -0,0 +1,16 @@
+var txtn1 = document.getElementById('txtn1')
+var txtn2 = document.getElementById('txtn2')
+var txtn3 = document.getElementById('txtn3')
+//convertendo txtn para numeral
+n1 = Number(txtn1.value)
+n2 = Number(txtn2.value)
+n3 = Number(txtn3.value)
+var visor = document.getElementById('visor')
+
+
+function enviar(){
+    document.write(`${n1}${n2}${n3}`)
+    while(n1,n2,n3){
+        
+    }
+}
