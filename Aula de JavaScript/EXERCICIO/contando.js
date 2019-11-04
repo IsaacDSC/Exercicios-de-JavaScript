84d4f902afa82f85f4005f9b87e5173369f2087f
IsaacDSC <56350331+IsaacDSC@users.noreply.github.com>
2019-11-04 06:16:59 -0300

    Adicionando site de estudo

---
+++ Aula de JavaScript/EXERCICIO/contando.js
@@ -0,0 +1,18 @@
+function abrirMenuExercicios() {
+    document.getElementById('modalNav').style.left = '0'
+}
+
+function fecharMenuExercicios() {
+    document.getElementsByTagName('h1').style.left = '-100%'
+}
+
+//adicionando modal exercicios/exercicios
+function abrirContando() {
+    document.getElementById('modalCompleta').style.left = '0'
+
+}
+
+function fecharContando() {
+    document.getElementById('modalCompleta').style.left = '-100%'
+
+}
